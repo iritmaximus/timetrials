e6337b0dd7dd12781c5a3fda061622fd5adc398b
iritmaximus <aaro.koivukangas@gmail.com>
2023-02-20 16:05:11 +0200

    fix: added secret key to flask

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from dotenv import load_dotenv
+from os import getenv
 
 load_dotenv()
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = getenv("SECRET_KEY")
+
 
 import flaskr.views
