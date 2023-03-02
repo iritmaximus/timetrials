b5882b298551e3f217d54ee9a9958b249125bd2a
iritmaximus <aaro.koivukangas@gmail.com>
2023-03-02 14:28:10 +0200

    fix: autoformatting

--- flaskr/games.py
+++ flaskr/games.py
@@ -6,6 +6,7 @@ def query_games():
     result = db.session.execute(sql).fetchall()
     return result
 
+
 def get_game_name(id: int):
     sql = f"SELECT name FROM games WHERE id=:id"
     result = db.session.execute(sql, {"id": id}).fetchone()
@@ -13,6 +14,7 @@ def get_game_name(id: int):
         return result[0]
     else:
         raise ValueError("No game with id found")
+
 
 def get_all_times_in_game(id: int):
     sql = "SELECT courses.name, times.course_id, users.username FROM times JOIN courses ON courses.id=times.course_id JOIN users ON users.id=times.user_id WHERE times.course_id=:id"
--- flaskr/views.py
+++ flaskr/views.py
@@ -28,15 +28,18 @@ def get_games():
         return render_template("games.html", games_exist=False)
     return render_template("games.html", games=games, games_exist=True)
 
+
 @app.route("/games/<int:game_id>")
 def get_game_by_id(game_id):
     try:
         game_id = int(game_id)
         game_name = get_game_name(game_id)
     except ValueError:
-        return render_template("error.html", message="Incorrect id, id not found or incorrect value")
+        return render_template(
+            "error.html", message="Incorrect id, id not found or incorrect value"
+        )
     times = get_all_times_in_game(game_id)
-    return render_template("game_id.html",  game_name=game_name, times=times)
+    return render_template("game_id.html", game_name=game_name, times=times)
 
 
 @app.route("/cups")
