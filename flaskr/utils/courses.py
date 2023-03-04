3cf0cccc36d378c02b1925cc7341a5fcf9e3e110
iritmaximus <aaro.koivukangas@gmail.com>
2023-03-04 19:31:55 +0200

    fix: courses can be clicked :)

--- flaskr/utils/courses.py
+++ flaskr/utils/courses.py
@@ -5,3 +5,16 @@ def query_courses():
     sql = "SELECT courses.name, games.name, cups.name FROM courses JOIN games ON games.id=courses.game_id JOIN cups ON cups.id=courses.cup_id"
     result = db.session.execute(sql).fetchall()
     return result
+
+
+def query_course_by_name(course_name: str):
+    sql = """
+    SELECT games.name, cups.name, times.timems, users.username
+    FROM times
+        JOIN games ON games.id=times.game_id
+        JOIN cups ON cups.id=times.cup_id
+        JOIN users ON users.id=times.user_id
+    WHERE times.course_id in (SELECT id FROM courses WHERE name=:course_name)
+    """
+    result = db.session.execute(sql, {"course_name": course_name}).fetchall()
+    return result
--- flaskr/views/courses.py
+++ flaskr/views/courses.py
@@ -1,7 +1,7 @@
 from flask import render_template
 
 from flaskr import app
-from flaskr.utils.courses import query_courses
+from flaskr.utils.courses import query_courses, query_course_by_name
 
 
 @app.route("/courses")
@@ -18,3 +18,13 @@ def get_course_by_id(course_id: int):
         course_id = int(course_id)
     except ValueError as e:
         return render_template("error.html", message=f"Incorrect id, {e}")
+
+
+@app.route("/api/course/<string:course_name>")
+def get_course_by_name(course_name: str):
+    try:
+        course_name = str(course_name).replace("%20", " ")
+        times = query_course_by_name(course_name)
+        return render_template("course_name.html", course_name=course_name, times=times)
+    except ValueError as e:
+        return render_template("error.html", message=f"Failed to fetch course, {e}")
