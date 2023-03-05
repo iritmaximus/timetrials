8614a95db50a5d8750e6fa5d6efe270b70ebedae
iritmaximus <aaro.koivukangas@gmail.com>
2023-03-05 13:43:10 +0200

    fix: removed flask imports

--- flaskr/views/home.py
+++ flaskr/views/home.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for, session
+from flask import render_template, redirect, url_for
 
 from flaskr import app
 from flaskr.database import test_db, create_db, reset_db
