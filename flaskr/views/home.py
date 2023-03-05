f835b6b197381afb96f061cf53f1989c2416ddf0
iritmaximus <aaro.koivukangas@gmail.com>
2023-03-05 23:09:15 +0200

    fix: removed harmful routes and added "info route" for /api

--- flaskr/views/home.py
+++ flaskr/views/home.py
@@ -1,7 +1,6 @@
 from flask import render_template, redirect, url_for
 
 from flaskr import app
-from flaskr.database import test_db, create_db, reset_db
 from flaskr.utils.user import check_session
 
 
@@ -12,18 +11,6 @@ def navigate():
     return render_template("home.html")
 
 
-# TODO remove these when ready
-@app.route("/db/init")
-def initialize_db():
-    return create_db()
-
-
-@app.route("/db/reset")
-def reset_database():
-    reset_db()
-    return "Initialization successful"
-
-
-@app.route("/db")
-def test_db_route():
-    return test_db()
+@app.route("/api")
+def api_help():
+    return "This is the api route. You will end up here from clicking a course, game or cup part of any time."
